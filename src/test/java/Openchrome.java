5c6d0ca32207f8973d6c948a5c60930d75fd2892
MohamedOuf52 <mohammedoff355@gmail.com>
2025-04-03 19:50:26 +0200

    initial commit

---
+++ src/test/java/Openchrome.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Openchrome {
+    @Test
+    public void openchrome(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+       // driver.navigate("https://www.google.com/ncr");
+    }
+}
