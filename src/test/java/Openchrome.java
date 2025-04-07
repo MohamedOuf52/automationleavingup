d6e038cf6b78519aad6f1f7011e4ef7aae8e0a6a
MohamedOuf52 <mohammedoff355@gmail.com>
2025-04-07 12:12:40 +0200

    initial commit

--- src/test/java/Openchrome.java
+++ src/test/java/Openchrome.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class OpenChrome {
@@ -10,16 +12,37 @@ public class OpenChrome {
      * Close Google Chrome
      *
      */
+@BeforeTest
+            public void setup() {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://duckduckgo.com/");
+    driver.manage().window().maximize();
 
-
+}
     @Test
+    public void openchrome() {
+        WebDriver driver = new ChromeDriver();
 
-    public void openchrome(){
+        assert driver.getTitle().equals("DuckDuckGo"); // Corrected expected title
+
+        driver.quit();
+    }
+
+    /* Open Google Chrome
+    Navigate to [https://duckduckgo.com/]
+    Assert that the DuckDuckGo logo is displayed
+    Close Google Chrome
+*/
+    @Test
+    public void openchrome2() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
-        driver.getTitle();
-     //   assert driver.getTitle().equals("Google");
 
-        driver.quit();    }
+        // Assert that the logo is displayed using a valid Selenium locator
+       assert driver.findElement(By.xpath("//section[@class='header_headerLeft__rW6nD header_headerSection___XMRI']//img")).isDisplayed();
+
+        driver.quit();
+    }
+
 
 }
--- src/test/java/Testwaits.java
+++ src/test/java/Testwaits.java
@@ -1,7 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class Testwaits {
     @Test
@@ -10,7 +15,22 @@ public class Testwaits {
         driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
         // implicity
-        driver.manage().timeouts().implicitlyWait(10, java.util.concurrent.TimeUnit.SECONDS);
-        driver.quit();
+        // driver.manage().timeouts().implicitlyWait(0, java.util.concurrent.TimeUnit.SECONDS);
+        //  driver.findElement(org.openqa.selenium.By.id("adder")).click();
+        // Explicit waits
+
+        driver.findElement(org.openqa.selenium.By.id("reveal")).click();
+
+        //.sendKeys("welcom to selnuim");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement inputElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("revealed")));
+        inputElement.sendKeys("welcome to selenium");
+
+
+        //  driver.findElement(org.openqa.selenium.By.id("reveal")).sendKeys("welcom to selnuim");
+        // driver.quit();
     }
+
+
 }
